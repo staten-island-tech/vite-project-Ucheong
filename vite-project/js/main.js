6c874e3e5599e4d2ea833a30f9bec289c60a4668
Ucheong <ucheongsou@gmail.com>
2022-12-01 09:52:47 -0500

    vite

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,6 @@
-import "./style.css";
+import "../styles/style.css";
 import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "../counter.js";
+import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
