0c35cb98098ae3f17e9c82b379765d2cb783f2dc
Ucheong <ucheongsou@gmail.com>
2022-12-07 10:04:00 -0500

    vite

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,6 +2,7 @@ import "../styles/style.css";
 import "./monkeys";
 import javascriptLogo from "./javascript.svg";
 import { setupCounter } from "./counter.js";
+import { monkeys } from "./monkeys";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -22,3 +23,20 @@ document.querySelector("#app").innerHTML = `
 `;
 
 setupCounter(document.querySelector("#counter"));
+monkeys
+  .filter((monkey) => monkey.gender === "male")
+  .forEach((monkey) => console.log(monkey.gender));
+monkeys
+  .filter((monkey) => monkey.age > "15")
+  .forEach((monkey) => console.log(monkey.age));
+monkeys
+  .filter((monkey) => monkey.available === true)
+  .forEach((monkey) => console.log(monkey.available));
+monkeys
+  .filter((monkey) => monkey.price > "$1000")
+  .forEach((monkey) => console.log(monkey.price));
+monkeys
+  .filter((monkey) => monkey.smart === "no")
+  .forEach((monkey) => console.log(monkey.smart));
+const monkeys2 = monkeys.price.map((monkey) => monkey * 2);
+console.log(monkeys2);
